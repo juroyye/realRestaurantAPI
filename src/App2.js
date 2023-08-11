f080f05db644a451489ce7a92e2b455529c59557
Justin M <manningjustin08@yahoo.com>
2023-08-11 13:24:43 -0400

    Fifth commit, implemented the location list API, the first API snippet I had added earlier in commit 4 was the restaurant list snippet

---
+++ src/App2.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const App2 = () => {
+   const [movies, setMovies] = useState([]);
+};
+
+export default App2;
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,27 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
+// locations list and API call
+const url = 'https://the-fork-the-spoon.p.rapidapi.com/locations/v2/list?google_place_id=ChIJu46S-ZZhLxMROG5lkwZ3D7k&geo_ref=false&geo_text=Roma%2C%20Metropolitan%20City%20of%20Rome%2C%20Italy&geo_type=locality';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bae3154e6amsh9de0e73ebe298f8p1def95jsn1394f149f348',
+		'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+
+
+// restaurant list and API calls
 // const url = 'https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list?queryPlaceValueCityId=348156&pageSize=10&pageNumber=1';
 // const options = {
 // 	method: 'GET',
