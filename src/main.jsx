9276285d7a2ec2055cfde01edbd2bd9d23e73535
Justin M <manningjustin08@yahoo.com>
2023-08-11 13:01:03 -0400

    Fourth commit, added API code snippet, info is displaying in console

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,36 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+// const url = 'https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list?queryPlaceValueCityId=348156&pageSize=10&pageNumber=1';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'bae3154e6amsh9de0e73ebe298f8p1def95jsn1394f149f348',
+// 		'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
+// class ExampleComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>This is a view created by a class component</h1>
+//       </div>
+//     );
+//   }
+// }
