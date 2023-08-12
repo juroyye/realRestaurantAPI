005dfe29b3f5860c5004d7651978d751c78a1697
Justin M <manningjustin08@yahoo.com>
2023-08-12 14:08:18 -0400

    Seventh commit, GOT DATA to DISPLAY IN CONSOLEgit add ., changed where I put API keys at

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,25 @@ function App() {
 
   return (
     <>
-    {/* <video className='bgVid' autoPlay loop muted>
+    <video className='bgVid' autoPlay loop muted>
       <source src={bgVideo} type='video/mp4' />
     </video>
-   */}
+  
 
 
-    {/* <h1 className='title'> ምግብ | 음식 | طعام</h1>
+    <h1 className='title'> ምግብ | 음식 | طعام</h1>
     <div className='fields'> 
      <input className='city' placeholder='...city, location, etc.'></input>
      <input className='keywords' placeholder='...italian food, bakeries, etc.'></input>
      </div>
-     <button className='submit'>Submit</button> */}
-
-
-
-     
+     <button className='submit'>Submit</button>
     </>
   )
 }
 
-export default App
+
+
+
+
+export default App;
 
