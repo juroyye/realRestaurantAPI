a5d6436f6e7a892ba578f24ba4f387deb4f384f9
Justin M <manningjustin08@yahoo.com>
2023-08-12 14:37:54 -0400

    Eight commit, combined files of App.jsx and main for simplicity

--- src/main.jsx
+++ src/main.jsx
@@ -9,41 +9,62 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-// locations list and API call
-fetch('https://the-fork-the-spoon.p.rapidapi.com/locations/v2/list?google_place_id=ChIJu46S-ZZhLxMROG5lkwZ3D7k&geo_ref=false&geo_text=Roma%2C%20Metropolitan%20City%20of%20Rome%2C%20Italy&geo_type=locality', {
-     "method": "GET",
-     "headers": {
-     'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com', 
- 		 'X-RapidAPI-Key': 'bae3154e6amsh9de0e73ebe298f8p1def95jsn1394f149f348',
+
+function main() {
+
+
+
+
+  // fetches the location data and console logs data
+// fetch('https://the-fork-the-spoon.p.rapidapi.com/locations/v2/list?google_place_id=ChIJu46S-ZZhLxMROG5lkwZ3D7k&geo_ref=false&geo_text=Roma%2C%20Metropolitan%20City%20of%20Rome%2C%20Italy&geo_type=locality', {
+//      "method": "GET",
+//      "headers": {
+//      'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com', 
+//  		 'X-RapidAPI-Key': 'bae3154e6amsh9de0e73ebe298f8p1def95jsn1394f149f348',
+// }
+// })
+// .then(response => response.json())
+// .then(response => {
+//   console.log(response);
+// })
+// .catch(err => {
+//   console.log(err);
+// });
+
+
+// fetches and consoles the restaurant list
+// fetch('https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list?queryPlaceValueCityId=348156&pageSize=10&pageNumber=1', {
+//      "method": "GET",
+//      "headers": {
+//      'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com', 
+//  		 'X-RapidAPI-Key': 'bae3154e6amsh9de0e73ebe298f8p1def95jsn1394f149f348',
+// }
+// })
+// .then(response => response.json())
+// .then(response => {
+//   console.log(response);
+// })
+// .catch(err => {
+//   console.log(err);
+// });
+
+  return (
+    <>
+
+
+    
+    
+    </>
+  
+  )
 }
-})
-.then(response => response.json())
-.then(response => {
-  console.log(response);
-})
-.catch(err => {
-  console.log(err);
-});
+main();
+
+export default main
 
 
 
 
-
-
-fetch('https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list?queryPlaceValueCityId=348156&pageSize=10&pageNumber=1', {
-     "method": "GET",
-     "headers": {
-     'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com', 
- 		 'X-RapidAPI-Key': 'bae3154e6amsh9de0e73ebe298f8p1def95jsn1394f149f348',
-}
-})
-.then(response => response.json())
-.then(response => {
-  console.log(response);
-})
-.catch(err => {
-  console.log(err);
-});
 
 
 
