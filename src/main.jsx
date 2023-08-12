74adef88af13d319d6209b8170d85aeffa130574
Justin M <manningjustin08@yahoo.com>
2023-08-12 15:16:47 -0400

    Ninth commit, added div that will hold displayed data

--- src/main.jsx
+++ src/main.jsx
@@ -12,47 +12,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 function main() {
 
-
-
-
-  // fetches the location data and console logs data
-// fetch('https://the-fork-the-spoon.p.rapidapi.com/locations/v2/list?google_place_id=ChIJu46S-ZZhLxMROG5lkwZ3D7k&geo_ref=false&geo_text=Roma%2C%20Metropolitan%20City%20of%20Rome%2C%20Italy&geo_type=locality', {
-//      "method": "GET",
-//      "headers": {
-//      'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com', 
-//  		 'X-RapidAPI-Key': 'bae3154e6amsh9de0e73ebe298f8p1def95jsn1394f149f348',
-// }
-// })
-// .then(response => response.json())
-// .then(response => {
-//   console.log(response);
-// })
-// .catch(err => {
-//   console.log(err);
-// });
-
-
-// fetches and consoles the restaurant list
-// fetch('https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list?queryPlaceValueCityId=348156&pageSize=10&pageNumber=1', {
-//      "method": "GET",
-//      "headers": {
-//      'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com', 
-//  		 'X-RapidAPI-Key': 'bae3154e6amsh9de0e73ebe298f8p1def95jsn1394f149f348',
-// }
-// })
-// .then(response => response.json())
-// .then(response => {
-//   console.log(response);
-// })
-// .catch(err => {
-//   console.log(err);
-// });
-
   return (
     <>
 
 
-    
+
     
     </>
   
